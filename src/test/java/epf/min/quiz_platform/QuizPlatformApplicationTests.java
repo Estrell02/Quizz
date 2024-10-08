c2fe88aff409b751e0b1db25eb6efcc89faff64c
Estrell02 <estrella.nounamo@institutsaintjean.org>
2024-10-08 14:36:24 +0200

    commit initial

---
+++ src/test/java/epf/min/quiz_platform/QuizPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package epf.min.quiz_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
